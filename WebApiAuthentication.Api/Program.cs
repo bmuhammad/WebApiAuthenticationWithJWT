7c61f8ad4a57d5f764767eee64a5b8076dde472c
bmuhammad <briancmuhammad@yahoo.com>
2025-03-14 18:28:03 -0700

    updated api  to make sure controller have authorized user by adding middle ware nuget package.  Configured in program.cs file the tokenvalidationparameters.

--- WebApiAuthentication.Api/Program.cs
+++ WebApiAuthentication.Api/Program.cs
@@ -1,14 +1,34 @@
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                            Convert.FromBase64String(
+                                builder.Configuration["Authentication:SecretForKey"]
+                                ?? throw new KeyNotFoundException("SecretForKey not found or invalid")))
+        };
+    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
